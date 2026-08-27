05fa91384a4e6acf4f61a0165efa76be3d03125b
v_tianmei <v_tianmei@tencent.com>
2026-08-27 12:22:44 +0800

    配置更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://meitian3611.github.io/",
-	base: "/",
+	// base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
